5266bca272d7222ad01e9cb8cb234fe91f4e1f11
Evan Morris <evancmorris17@gmail.com>
2019-11-16 12:28:56 -0800

    Finalized Classifier File, Modified Plotter to accept stage data
    Includes output file from classifer

--- data/ParkinsonGaitPlotter.py
+++ data/ParkinsonGaitPlotter.py
@@ -2,35 +2,39 @@ import matplotlib.pyplot as plt
 import csv
 
 def main():
-	#initialize lists to hold data
-	time = []
-	leftFootForce = []
-	rightFootForce = []
+    #initialize lists to hold data
+    time = []
+    leftFootForce = []
+    rightFootForce = []
+    stage = []
 
-	#open data file
-	with open('JuPt01_06_csv.csv','r') as csvFile:
+    #open data file
+    with open('output.csv','r') as csvFile:
 
-	    plots = csv.reader(csvFile, delimiter=',')
-	    #read data into memory
-	    for column in plots:
-	        time.append(float(column[0]))
-	        leftFootForce.append(float(column[1]))
-	        rightFootForce.append(float(column[2]))
+        plots = csv.reader(csvFile, delimiter=',')
+        #read data into memory
+        for column in plots:
+            time.append(float(column[0]))
+            leftFootForce.append(float(column[1]))
+            rightFootForce.append(float(column[2]))
+            stage.append(float(column[3]))
 
-	#set window size
-	plt.figure(figsize=(15,6))
-	#plot left foot force by x
-	plt.plot(time, leftFootForce, color='blue', label='Left Foot')
-	#plot right foot force by y
-	plt.plot(time, rightFootForce, color='red', label='Right Foot')
-	#set range for x axis (X axis is truncated for readability)
-	plt.xlim([0.0, 30.0])
-	
-	plt.xlabel('Time (seconds)')
-	plt.ylabel('Force (N)')
-	plt.title('Gait in Parkinson\'s Disease')
-	plt.legend()
-	plt.show()
+    #set window size
+    plt.figure(figsize=(15,6))
+    #plot left foot force by x
+    plt.plot(time, leftFootForce, color='blue', label='Left Foot')
+    #plot right foot force by y
+    plt.plot(time, rightFootForce, color='red', label='Right Foot')
+    #plot stage 
+    plt.plot(time, stage, color="green", label='Stage')
+    #set range for x axis (X axis is truncated for readability)
+    plt.xlim([0.0, 30.0])
+    
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Force (N)')
+    plt.title('Gait in Parkinson\'s Disease')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
-	main()
+    main()
--- data/ParkinsonGaitPlotterClassifier.py
+++ data/ParkinsonGaitPlotterClassifier.py
@@ -4,7 +4,7 @@ import csv
 def main():
     #initialize lists to hold data
     data = []
-    limit = 2
+    limit = 5
     slopes = []
     #open data file
     with open('JuPt01_06_csv.csv','r') as csvFile:
@@ -17,18 +17,44 @@ def main():
             time = float(column[0])
             left = float(column[1])
             right = float(column[2])
-            if time != 0:
-                slopes.append(left/time)
-                if i > 1:
-                    if abs(slopes[i-1] - slopes[i-2]) > limit:
-                        if stage == 5:
-                            stage = 1
-                        stage += 1
-                data.append([time, left, right, stage])
-            i += 1
+            if time != 0: # ignore first irrelevant line
+                if i > 1: # wait for two data points
 
-    for line in data:
-        print(line)
+                    # gets change in force for left and right foot 
+                    leftDiff = left - data[i-2][1]
+                    rightDiff = right - data[i-2][2]
+                    slopes.append([leftDiff, rightDiff]) # append to list to compare to later
+
+                    # booleans if the change is over the limit (True if Rising or Falling quickly)
+                    MAG_COND_LEFT = abs(slopes[i-2][0]) > limit 
+                    MAG_COND_RIGHT = abs(slopes[i-2][1]) > limit  
+                    
+                    # State Machine
+                    if stage == 1: # right increasing force > left
+                        if not MAG_COND_LEFT: # checks for when left foot is in air
+                            stage = 2
+                    elif stage == 2: # right const force
+                        if MAG_COND_LEFT and MAG_COND_RIGHT: # checks if both feet are changing
+                            stage = 3
+                    elif stage == 3: # right decreasing force > left
+                        if left > right: # checks for when left force overtakes right force
+                            stage = 4
+                    elif stage == 4: # right decreasing force < left
+                        if not MAG_COND_RIGHT: # checks for when right foot is in air
+                            stage = 5
+                    elif stage == 5: # right no force
+                        if MAG_COND_RIGHT and MAG_COND_LEFT: # checks for when both feet are changing
+                            stage = 6
+                    elif stage == 6: # right increasing force < left
+                        if left < right: # checks for when right force overtakes left 
+                            stage = 1
+                data.append([time, left, right, stage*100]) # stage multipied by 100 for visibility on graphs
+            i += 1
+    
+    # write to output file
+    with open('output.csv','w', newline="") as result_file:
+        wr = csv.writer(result_file)
+        wr.writerows(data)
     
 if __name__ == '__main__':
     main()
