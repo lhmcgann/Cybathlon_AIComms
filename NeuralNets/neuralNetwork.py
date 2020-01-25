55caa81c4c9c3360b8a156238f10c529156424d9
Dimitri Charitou <dimitricharitou@gmail.com>
2020-01-25 14:35:34 -0800

    Developed loadData function

--- NeuralNets/neuralNetwork.py
+++ NeuralNets/neuralNetwork.py
@@ -1,24 +1,52 @@
+'''
+* Neural Network
+*
+* Dimitri Charitou, Evan Morris, Dylan Mooers
+* Cal Poly QL+ Cybathlon
+* 01/25/2020
+'''
+
 import matplotlib.pyplot as plt
 import csv
 import tensorflow as tf
+import numpy as np
+
+# string -> (tf.data.Dataset, tf.data.Dataset, tf.data.Dataset])
+# Returns a list of three dataset objects, [training dataset, validation dataset, testing dataset]
+# Arrays are broken up into 85% training, 5% validation, 10% testing
+def loadData(filename):
+	# change these variables for tweak percentages
+	TRAINING_PERCENTAGE = 0.85
+	VALIDATION_PERCENTAGE = 0.5
+	TESTING_PERCENTAGE = 0.10
+
+	dataset = []
+	trainingDataset = []
+	validationDataset = []
+	testingDataset = []
+
+	with open(filename,'r') as csvFile :
+		data = csv.reader(csvFile, delimiter=',')
+		
+		# read data into memory
+		for column in data :
+			dataset.append([float(column[1]), float(column[2]), float(column[4])])
+
+		# partitions data into three distinct chunks
+		trainingDataset = dataset[0 : int(len(dataset) * TRAINING_PERCENTAGE)]
+		validationDataset = dataset[int(len(dataset) * TRAINING_PERCENTAGE) : 
+			int(len(dataset) * VALIDATION_PERCENTAGE)]
+		testingDataset = dataset[int(len(dataset) * VALIDATION_PERCENTAGE) :]
+
+	
+	return (tf.data.Dataset.from_tensor_slices(trainingDataset), 
+		tf.data.Dataset.from_tensor_slices(validationDataset),
+		tf.data.Dataset.from_tensor_slices(testingDataset))
 
 
-def main() :
-    dataSet = []
+def main():
+	dataset = loadData('../data/outputWithRegression.csv')
 
-    with open('outputWithRegression.csv','r') as csvFile :
-        data = csv.reader(csvFile, delimiter=',')
-        
-        for column in data :
-            dataSet.append([float(column[1]),
-            float(column[2]), 
-            float(column[4])])
-    
-    
-    dataset = tf.data.Dataset.from_tensor_slices(dataSet)
-    for element in dataset :
-        print(element)
 
-    #print(dataSet[0])
-
-main()
+if __name__ == '__main__':
+	main()
