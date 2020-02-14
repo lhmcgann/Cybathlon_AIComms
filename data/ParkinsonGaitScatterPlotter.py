688f40ab0791cd8e54ab29303fa86f7221593836
lhmcgann <hailey.mcgann@gmail.com>
2020-02-13 18:45:25 -0800

    pulled changes from svm

--- data/ParkinsonGaitScatterPlotter.py
+++ data/ParkinsonGaitScatterPlotter.py
@@ -2,7 +2,7 @@
 #
 # Dimitri Charitou
 # QL+_Cybathlon
-	
+
 
 import matplotlib.pyplot as plt
 import csv
@@ -24,22 +24,22 @@ def main():
 	rightFootForce600 = []
 
 	#open data file
-	with open('output.csv','r') as csvFile:
+	with open('usable_input_data.csv','r') as csvFile:
 
 		plots = csv.reader(csvFile, delimiter=',')
 		#read data into 6 gait cycle lists which hold vectors (x=left foot force, y=right foot force)
 		for column in plots:
 			if (int(column[3]) == 100):
 				leftFootForce100.append(float(column[1]))
-				rightFootForce100.append(float(column[2]))	  
-				
+				rightFootForce100.append(float(column[2]))
+
 			elif (int(column[3]) == 200):
 				leftFootForce200.append(float(column[1]))
 				rightFootForce200.append(float(column[2]))
 
 			elif (int(column[3]) == 300):
 				leftFootForce300.append(float(column[1]))
-				rightFootForce300.append(float(column[2]))	  
+				rightFootForce300.append(float(column[2]))
 
 			elif (int(column[3]) == 400):
 				leftFootForce400.append(float(column[1]))
@@ -47,7 +47,7 @@ def main():
 
 			if (int(column[3]) == 500):
 				leftFootForce500.append(float(column[1]))
-				rightFootForce500.append(float(column[2]))	  
+				rightFootForce500.append(float(column[2]))
 
 			elif (int(column[3]) == 600):
 				leftFootForce600.append(float(column[1]))
@@ -55,20 +55,20 @@ def main():
 
 	#set window size
 	plt.figure(figsize=(15,6))
-	
+
 	#plot points by left and right foot forces
 	plt.scatter(leftFootForce100, rightFootForce100, c='red')
 	plt.scatter(leftFootForce200, rightFootForce200, c='green')
 
 	#uncomment these lines for more data graphing
-	#plt.scatter(leftFootForce300, rightFootForce300, c='blue')
-	#plt.scatter(leftFootForce400, rightFootForce400, c='yellow')
-	#plt.scatter(leftFootForce500, rightFootForce500, c='orange')
-	#plt.scatter(leftFootForce600, rightFootForce600, c='purple')
+	plt.scatter(leftFootForce300, rightFootForce300, c='blue')
+	plt.scatter(leftFootForce400, rightFootForce400, c='yellow')
+	plt.scatter(leftFootForce500, rightFootForce500, c='orange')
+	plt.scatter(leftFootForce600, rightFootForce600, c='purple')
 
 	#set range for x axis (X axis is truncated for readability)
 	plt.xlim([0.0, 50.0])
-	
+
 	plt.xlabel('Left Force (N)')
 	plt.ylabel('Right Force (N)')
 	plt.title('Gait in Parkinson\'s Disease')
