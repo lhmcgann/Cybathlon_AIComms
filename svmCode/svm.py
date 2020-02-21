f47657ee3dd3438f8e61d0a8ac526239c78a1dd2
MaxLewter16 <56984396+MaxLewter16@users.noreply.github.com>
2020-02-20 17:27:57 -0800

    Adds confusion matrix

--- svmCode/svm.py
+++ svmCode/svm.py
@@ -2,6 +2,11 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 import csv
 from matplotlib import pyplot as plt
+import seaborn as sn
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import sklearn.metrics
+import numpy as np
 # take as input two arrays: an array X of size [n_samples, n_features] holding
 #   the training samples, and an array y of class labels (strings or integers),
 #   size [n_samples]
@@ -37,6 +42,16 @@ print(clf.n_support_)
 
 # After being fitted, the model can then be used to predict new values:
 predictions = clf.predict(X_test)
+
+print(sklearn.metrics.classification_report(y_test, predictions, digits=3 ))
+# Prints test and predicted data into a heatmap of a confusion matrix
+df_cm = pd.DataFrame(confusion_matrix(y_test, predictions))
+sn.set(font_scale=1) # for label size
+confusion = sn.heatmap(df_cm, annot=True, annot_kws={"size": 12}, xticklabels=(100, 200, 300, 400, 500, 600),
+           yticklabels=(100,200,300,400,500,600), cbar=False, ) # font size
+confusion.set(title= 'SVM Confusion Matrix',
+              xlabel="Predictions",
+              ylabel='Real Values',)
 
 # print the accuracy score
 print('Score: ', clf.score(X_test, y_test))
